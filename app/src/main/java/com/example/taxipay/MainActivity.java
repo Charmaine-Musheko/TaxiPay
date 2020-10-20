8af4c74f08aa3c79218917cb4f1a6fe7c6d25c48
Charmaine <chamainemush@gmail.com>
2020-10-20 15:17:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/taxipay/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.taxipay;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
+
+import com.google.zxing.Result;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.security.Permission;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView scannerView;
+    private TextView txtResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scannerView = new ZXingScannerView(this);
+        scannerView = (ZXingScannerView)findViewById(R.id.zxscan);
+        txtResult = (TextView)findViewById(R.id.txt_result);
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                    scannerView.setResultHandler(MainActivity.this);
+                    scannerView.startCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity.this, "You must ask for permission", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        scannerView.stopCamera();
+        super.onDestroy();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        txtResult.setText(rawResult.getText());
+
+    }
+}
