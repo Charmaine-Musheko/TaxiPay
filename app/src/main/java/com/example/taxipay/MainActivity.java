ec06f6fea8d931912417ed5731e4da28ee4bda3a
Charmaine <chamainemush@gmail.com>
2020-10-22 15:44:08 +0200

    1

--- app/src/main/java/com/example/taxipay/MainActivity.java
+++ app/src/main/java/com/example/taxipay/MainActivity.java
@@ -4,9 +4,13 @@ package com.example.taxipay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.zxing.Result;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -30,5 +34,10 @@ public class MainActivity extends AppCompatActivity {
         scannerView = (ZXingScannerView) findViewById(R.id.zxscan);
         txtResult = (TextView) findViewById(R.id.txt_result);
 
+    }
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/taxipay/Register.java
+++ app/src/main/java/com/example/taxipay/Register.java
@@ -1,14 +1,83 @@
 package com.example.taxipay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+    EditText mName, mpassword, memail, mphone;
+    Button mregister;
+    TextView mtext, mtext_account, mlogin;
+    FirebaseAuth fAuth;
+    ProgressBar mProgressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mtext = findViewById(R.id.text_taxipay);
+        mtext_account = findViewById(R.id.text_newaccount);
+        mName = findViewById(R.id.fullName);
+        memail = findViewById(R.id.Email);
+        mpassword = findViewById(R.id.password);
+        mphone = findViewById(R.id.phone_number);
+        mregister = findViewById(R.id.register_button);
+        mlogin = findViewById(R.id.login_again);
+        mProgressBar = findViewById(R.id.progressBar);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+        mregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = memail.getText().toString().trim();
+                String password = mpassword.getText().toString().trim();
+                if (TextUtils.isEmpty(email)){
+                    memail.setError("Invalid E-mail Address");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mpassword.setError("Password is required");
+                    return;
+                }
+                if(password.length() < 6){
+                    mpassword.setError(" Password must be >= 6 Characters");
+                    return;
+                }
+                mProgressBar.setVisibility(View.VISIBLE);
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register.this, "User created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Register.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
     }
 }
